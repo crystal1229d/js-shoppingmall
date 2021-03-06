10a59803fd991ad220be385d9680b4faf78baf4a
crystal1229d <crystal1229d@naver.com>
2021-03-07 02:11:39 +0900

    2021-03-07(SUN)

--- src2/main.js
+++ src2/main.js
@@ -22,16 +22,38 @@ function createHTMLString(item) {
     `
 }
 
+// Handle button click
 function onButtonClick(event, items) {
+    
+    // (방법 1) 버튼이 클릭될 때마다 컨테이너에 해당하는 데이터들을 새로 가져와 넣는다
     const dataset = event.target.dataset
     const key = dataset.key
     const value = dataset.value
     
-    if (key == null || value == null) {
-        return 
-    }
+    if (key == null || value == null) { return }
 
     displayItems(items.filter(item => item[key] === value))
+
+    // (방법 2) (개선) 버튼이 클릭될 때마다 기존에 존재하던 데이터 중 해당하는 데이터만 보이게 한다
+    // const target = event.target
+    // const key = target.dataset.key
+    // const value = target.dataset.value
+
+    // if (key == null || value == null) { return }
+
+    // updateItems(items, key, value)
+
+}
+
+// Make the items matching {key: value} invisible
+function updateItems(items, key, value) {
+    items.forEach(item => {
+        if (item.dataset[key] === value) {
+            item.classList.remove('invisible')
+        } else {
+            item.classList.add('invisible')
+        }
+    })
 }
 
 function setEventListeners(items) {
